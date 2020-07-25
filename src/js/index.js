b1df2e3456d86c570b87e22016ac9c1b8e5c45ba
Eliza Osinska <eliza.osinska@op.pl>
2020-07-25 21:30:45 +0200

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
@@ -10,17 +10,25 @@ console.log('HELLO 🚀')
 
 const glassCounter = document.querySelector('.glass__counter--js');
 const buttonAdd = document.querySelector('.button-add--js');
-const buttonRemo = document.querySelector('.button-remove--js');
+const buttonRemove = document.querySelector('.button-remove--js');
 const key = new Date().toISOString().slice(0, 10);
 
-console.log(key);
-
 let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
 
 buttonAdd.addEventListener('click', () => {
     currentGlassCounter++;
     glassCounter.innerHTML = currentGlassCounter;
-
+    localStorage.setItem(key, currentGlassCounter);
 });
 
 buttonRemove.addEventListener('click', () => {
@@ -28,4 +36,5 @@ buttonRemove.addEventListener('click', () => {
         currentGlassCounter--;
     }
     glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 })
